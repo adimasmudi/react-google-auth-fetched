b82c6e5ddde9b5688eb2e972e0c3ca1e80cd02eb
adimasmudi <adimasmudi@gmail.com>
2023-01-17 08:29:53 +0700

    second commit

--- src/GoogleCallback.js
+++ src/GoogleCallback.js
@@ -3,8 +3,6 @@ import React, { useState, useEffect } from "react";
 function GoogleCallback() {
   const [data, setData] = useState({});
 
-  // On page load, we take "search" parameters
-  // and proxy them to /api/auth/callback on our Laravel API
   useEffect(() => {
     fetch(`http://api.lenjelenanmadura.id/api/auth/signup/callback`, {
       headers: {
